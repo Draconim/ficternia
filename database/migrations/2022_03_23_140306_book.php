e4482fe89921710c8878a8ec6d52e6e0e048fe7d
Barankai László Ferenc <barankailaci@gmail.com>
2022-03-24 16:06:39 +0100

    Created project

---
+++ database/migrations/2022_03_23_140306_book.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table){
+            $table->id();
+            $table->foreignId('username');
+            $table->string('title');
+            $table->string('cover');
+            $table->string('description');
+            $table->foreignId('genre_id')->unique();
+            $table->int('views');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+};
